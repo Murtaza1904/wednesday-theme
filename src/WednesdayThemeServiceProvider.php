84011eaf338c5f37c30be7bd805ef61abb415a2d
Muhammad Murtaza <muhammadmurtaza1904@gmail.com>
2025-10-24 15:55:03 +0500

    assets path issue.

--- src/WednesdayThemeServiceProvider.php
+++ src/WednesdayThemeServiceProvider.php
@@ -11,13 +11,13 @@ final class WednesdayThemeServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->publishes([
-            __DIR__.'/../public/wednesday-theme/css' => public_path('vendor/murtaza1904/wednesday-theme/resources/assets/css'),
-            __DIR__.'/../resources/js' => public_path('vendor/murtaza1904/wednesday-theme/resources/js'),
+            __DIR__ . '/../resources/css' => public_path('vendor/murtaza1904/wednesday-theme/resources/css'),
+            __DIR__ . '/../resources/js'  => public_path('vendor/wednesday-theme/resources/js'),
         ], 'wednesday-theme-assets');
     }
 
     public function register(): void
     {
-        $this->app->singleton(ThemeManager::class, fn() => new ThemeManager('wednesday'));
+        $this->app->singleton(ThemeManager::class, fn () => new ThemeManager('wednesday'));
     }
 }
